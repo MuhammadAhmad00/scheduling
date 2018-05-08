24ca8474caa3db8a95da98f3a77f4c21fa582f07
Muhammad Ahmad <37689069+MuhammadAhmad00@users.noreply.github.com>
2018-05-08 20:27:46 +0500

    Update sjf.py

--- sjf.py
+++ sjf.py
@@ -4,6 +4,7 @@ burst_time = []
 arrival_time = []
 finish_time =[]
 waiting_time =[]
+sum =int(0)
 print "Enter the number of processes"
 n = int(input())
 print "Enter the Name of the processes"
@@ -14,7 +15,7 @@ print "Enter the burst time of the processes"
 burst_time = [int(input()) for i in range(n)]
 for i in range(n):
     for j in range(n-i-1):
-        if(burst_time[j]>burst_time[j+1]):
+        if burst_time[j] > burst_time[j+1]:
             temp2=burst_time[j]
             burst_time[j]=burst_time[j+1]
             burst_time[j]=temp2
@@ -24,11 +25,16 @@ for i in range(n):
 print "Process Name"+"\t"+"Arrival time"+"\t"+"Burst time"
 for i in range(n):
     print name[i],"\t\t\t",arrival_time[i],"\t\t\t\t",burst_time[i]
+    finish_time = [int(arrival_time[i] + burst_time[i]) for i in range(n)]
+    waiting_time=[int(finish_time[i]-arrival_time[i]) for i in range(n)]
 for i in range(n):
-    finish_time[i] = arrival_time[i] + burst_time[i]
-    print "waiting time",waiting_time
-    sum+=waiting_time[i]
+        sum += int(waiting_time[i])
+print "waiting time of Process"
+for i in range(n):
+    print name[i],"\t\t",waiting_time[i]
 print "Average waiting time ",sum/n
+
+
 
 
 
